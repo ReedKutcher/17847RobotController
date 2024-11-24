bee54c6a31bdd1db0c44588af7c2da4079c96457
Reed Kutcher <154007984+ReedKutcher@users.noreply.github.com>
2024-11-23 21:41:38 -0500

    Programmed During Scrimmage
    Mapped TeleOp Controls for Current Configuration
    - Before today BasicDrive.java only had controls for drivetrain (Left Joystick: Move / Strafe | Right Joystick: Turn)
    - Added controls for:
        - Intake arm (Triggers: extend or retract arm)
        - Outtake viper slides (y & b buttons: retract or extend viper slide)
        - Outtake bucket (bumpers: flip / turn outtake bucket)
        - Intake wheel (a & x buttons (toggle): suck sample in / spit sample out)
    
    Started to Translate MeepMeep and Autonomous Pseudo-code into real Autonomous code
    - Followed instructions on https://rr.brott.dev/docs/v1-0/guides/centerstage-auto/
    - Created Classes and Actions to define custom movement for non-drivetrain mechanisms in AutoRight.java
    
    Future Plans:
    - Improve control in BasicDrive.java, as coding was a bit rushed and driving is clunky
    - Figure out how to translate MeepMeep paths into paths for Autonomous, including custom movement. Ideally this should be done without too much spaghetti code or repetition.
    - Finish translating MeepMeep paths to robot paths for AutoRight.java, then do the same for a new class AutoLeft.java.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRight.java
@@ -1,4 +1,131 @@
 package org.firstinspires.ftc.teamcode;
 
-public class AutoRight {
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous
+public class OdometryTest extends LinearOpMode {
+    public class Arm{
+        private DcMotorEx arm;
+
+        public Arm(HardwareMap hardwareMap){
+            arm = hardwareMap.get(DcMotorEx.class, "inarm");
+            arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+
+        public class ExtendArm implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    arm.setPower(0.5);
+                    initialized = true;
+                }
+
+                double pos = arm.getCurrentPosition();
+                packet.put("armPos", pos);
+                if (pos < 200.0) {
+                    return true;
+                } else {
+                    arm.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action extendArm(){
+            return new ExtendArm();
+        }
+
+        public class RetractArm implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    arm.setPower(-0.5);
+                    initialized = true;
+                }
+
+                double pos = arm.getCurrentPosition();
+                packet.put("armPos", pos);
+                if (pos > 50.0) {
+                    return true;
+                } else {
+                    arm.setPower(0);
+                    return false;
+                }
+            }
+        }
+        public Action retractArm(){
+            return new RetractArm();
+        }
+    }
+
+    public class InServo{
+        private Servo inServo;
+
+        public InServo(HardwareMap hardwareMap){
+            inServo = hardwareMap.get(Servo.class, "inwheel");
+        }
+
+        public class SpinIn implements Action{
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                inServo.setPosition(1);
+                return false;
+            }
+        }
+        public Action spinIn(){
+            return new SpinIn();
+        }
+
+        public class SpinStop implements Action{
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                inServo.setPosition(0.5);
+                return false;
+            }
+        }
+        public Action spinStop(){
+            return new SpinStop();
+        }
+
+        public class SpinOut implements Action{
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                inServo.setPosition(0);
+                return false;
+            }
+        }
+        public Action spinOut(){
+            return new SpinOut;
+        }
+    }
+
+    @Override
+    public void runOpMode() {
+        Pose2d initialPose = new Pose2d(24, -61, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        Arm arm = new Arm(hardwareMap);
+        InServo inServo = new InServo(hardwareMap);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDrive.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
@@ -13,9 +15,22 @@ public class BasicDrive extends LinearOpMode {
     private DcMotor bl;
     private DcMotor br;
 
+    private DcMotorEx inarm;
+    private DcMotor outslide;
+    private Servo outl;
+    private Servo outr;
+    private Servo inwheel;
+
     double pwrX;
     double pwrY;
     double turn;
+    double armPos;
+    double slidiness;
+    double outrotation;
+    boolean inForward = false;
+    boolean inBackward = false;
+    boolean aHeld = false;
+    boolean xHeld = false;
 
     @Override
     public void runOpMode() {
@@ -24,15 +39,18 @@ public class BasicDrive extends LinearOpMode {
         bl = hardwareMap.get(DcMotor.class, "bl");
         br = hardwareMap.get(DcMotor.class, "br");
 
-        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        inarm = hardwareMap.get(DcMotorEx.class, "inarm");
+        outslide = hardwareMap.get(DcMotor.class, "outslide");
+        outl = hardwareMap.get(Servo.class, "outl");
+        outr = hardwareMap.get(Servo.class, "outr");
+        inwheel = hardwareMap.get(Servo.class, "inwheel");
 
         fl.setDirection(DcMotorSimple.Direction.FORWARD);
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         bl.setDirection(DcMotorSimple.Direction.FORWARD);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        outslide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
@@ -41,10 +59,49 @@ public class BasicDrive extends LinearOpMode {
             pwrY = gamepad1.left_stick_y;
             turn = gamepad1.right_stick_x;
 
+            armPos = (gamepad1.right_trigger - gamepad1.left_trigger);
+            outrotation = ((gamepad1.right_bumper ? 0.5 : 0) - (gamepad1.left_bumper ? 0.5 : 0));
+            slidiness = ((gamepad1.y ? 1 : 0) - (gamepad1.b ? 1 : 0));
+
+            if(gamepad1.a && !aHeld){
+                if(inForward){
+                    inwheel.setPosition(0.5);
+                    inForward = false;
+                }else{
+                    inForward = true;
+                    inBackward = false;
+                    inwheel.setPosition(1);
+                }
+                aHeld = true;
+            }
+            if(gamepad1.x && !xHeld){
+                if(inBackward){
+                    inwheel.setPosition(0.5);
+                    inBackward = false;
+                }else{
+                    inForward = false;
+                    inBackward = true;
+                    inwheel.setPosition(-1);
+                }
+                xHeld = true;
+            }
+
+            if(!gamepad1.a){
+                aHeld = false;
+            }
+            if(!gamepad1.b){
+                xHeld = false;
+            }
+
             fl.setPower(pwrY-pwrX-turn);
             fr.setPower(pwrY+pwrX+turn);
             bl.setPower(pwrY+pwrX-turn);
             br.setPower(pwrY-pwrX+turn);
+
+            outslide.setPower(slidiness);
+            inarm.setPower(armPos/3);
+            outl.setPosition(0.5+outrotation);
+            outr.setPosition(0.5-outrotation);
         }
     }
 }
